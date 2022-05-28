f41e210332c4d155583a7e0e359067c54ef3a5f5
LeeJaeHoon <dlwogns3413@naver.com>
2022-05-29 03:56:18 +0900

    feat: 404 page and Redirect setting

---
+++ src/pages/client/restaurants.tsx
@@ -0,0 +1,3 @@
+export const Restaurants = () => {
+  return <h1>Restaurant</h1>;
+};
--- src/routers/logged-out-router.tsx
+++ src/routers/logged-out-router.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { NotFound } from "../pages/404";
 import { CreateAccount } from "../pages/create-account";
 import { Login } from "../pages/login";
 
@@ -6,8 +7,9 @@ export const LoggedOutRouter = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} />
         <Route path="/create-account" element={<CreateAccount />} />
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
